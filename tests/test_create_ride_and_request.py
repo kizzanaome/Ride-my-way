c273cee9f7dcbbeed3a94eccdb714c5a2f3ff088
kizzanaome <naomekizzan@gmail.com>
2018-06-24 17:02:25 -0700

    tests for the ride offers

---
+++ tests/test_create_ride_and_request.py
@@ -0,0 +1,27 @@
+import unittest
+from unittest import TestCase
+from app.rides.models import Ride, rides_list, RequestForRide,requests
+
+
+class TestRideViews(unittest.TestCase):
+
+    def test_add_ride(self):
+        self.assertEqual(len(rides_list),0)
+        ride= Ride(1,"name","origin","destination")
+        ride.create_ride()
+        self.assertEqual(len(rides_list),1)
+
+class TestRequestsForRides(unittest.TestCase):
+
+    def test_create_requests(self):
+        self.assertEqual(len(requests),0)
+        request= RequestForRide(1,"ride_id","name","accepted")
+        request.create_request()
+        self.assertEqual(len(requests),1)
+
+    
+
+
+
+
+    
